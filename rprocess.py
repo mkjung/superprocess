02961697726e2b7c651da2ce1dd63a19b4c20717
Stephen Drake <steve@synergyconsultingnz.com>
2014-12-01 15:27:44 +1300

    Use connection objects to provide implementation
    Use connection objects that implement Popen etc instead of only
    transforming args before passing to subprocess.  This makes it
    possible to use other implementations not based on subprocess,
    eg using paramiko.

--- rprocess.py
+++ rprocess.py
@@ -1,63 +1,83 @@
 import pipes
 import subprocess
 
-
-# prepare command for remote execution
-def remote_cmd(cmd, netloc=None, remote_shell=None):
-	# don't attempt remote shell if command or netloc empty
-	if not cmd or not netloc:
-		return cmd
+# open a connection that can be used to execute processes
+def connect(netloc):
+	# use local connection if netloc is empty
+	if not netloc:
+		return local_connection
 
 	# split netloc
 	username, _, hostname = netloc.rpartition('@')
 
 	# don't use remote shell for localhost unless user is specified
 	if not username and hostname in {'localhost', '127.0.0.1'}:
-		return cmd
+		return local_connection
 
-	# use ssh as default remote shell
-	if not remote_shell:
-		rsh_cmd = ['ssh']
-	elif isinstance(remote_shell, basestring):
-		rsh_cmd = [remote_shell]
-	else:
-		rsh_cmd = list(remote_shell)
+	return RemoteShellConnection(hostname, username)
 
-	# set remote user and host
-	if username:
-		rsh_cmd.append('-l')
-		rsh_cmd.append(username)
-	rsh_cmd.append(hostname)
+# Use subprocess module directly for local commands
+local_connection = subprocess
 
-	# quote command for remote shell if provided as list
-	if isinstance(cmd, basestring):
-		rsh_cmd.append(cmd)
-	else:
-		rsh_cmd.extend(pipes.quote(x) for x in cmd)
+class RemoteShellConnection(object):
+	def __init__(self, hostname, username=None, remote_shell=None):
+		# use ssh as default remote shell
+		if not remote_shell:
+			remote_shell = ['ssh']
+		elif isinstance(remote_shell, basestring):
+			remote_shell = [remote_shell]
+		else:
+			remote_shell = list(remote_shell)
 
-	return rsh_cmd
+		# set remote user and host
+		if username:
+			remote_shell.append('-l')
+			remote_shell.append(username)
+		remote_shell.append(hostname)
+
+		# store remote shell command list
+		self.remote_shell = remote_shell
+
+	# call subprocess function after adjusting cmd to run in remote shell
+	def _call(self, f, cmd, *args, **kwargs):
+		# quote command for remote shell if provided as list
+		if isinstance(cmd, basestring):
+			cmd = [cmd]
+		else:
+			cmd = [pipes.quote(x) for x in cmd]
+		cmd = self.remote_shell + cmd
+
+		# shell option not suitable for command list
+		kwargs['shell'] = False
+
+		# call function with updated args
+		return f(cmd, *args, **kwargs)
+
+	def call(self, *args, **kwargs):
+		return self._call(subprocess.call, *args, **kwargs)
+
+	def check_call(self, *args, **kwargs):
+		return self._call(subprocess.check_call, *args, **kwargs)
+
+	def check_output(self, *args, **kwargs):
+		return self._call(subprocess.check_output, *args, **kwargs)
+
+	def Popen(self, *args, **kwargs):
+		return self._call(subprocess.Popen, *args, **kwargs)
 
 # subprocess overrides
 def Popen(*popenargs, **kwargs):
-	netloc = kwargs.pop('netloc', None)
-	remote_shell = kwargs.pop('remote_shell', None)
-	cmd = remote_cmd(popenargs[0], netloc, remote_shell)
-	return subprocess.Popen(cmd, *popenargs[1:], **kwargs)
+	connection = connect(kwargs.pop('netloc', None))
+	return connection.Popen(*popenargs, **kwargs)
 
 def call(*popenargs, **kwargs):
-	netloc = kwargs.pop('netloc', None)
-	remote_shell = kwargs.pop('remote_shell', None)
-	cmd = remote_cmd(popenargs[0], netloc, remote_shell)
-	return subprocess.call(cmd, *popenargs[1:], **kwargs)
+	connection = connect(kwargs.pop('netloc', None))
+	return connection.call(*popenargs, **kwargs)
 
 def check_call(*popenargs, **kwargs):
-	netloc = kwargs.pop('netloc', None)
-	remote_shell = kwargs.pop('remote_shell', None)
-	cmd = remote_cmd(popenargs[0], netloc, remote_shell)
-	return subprocess.check_call(cmd, *popenargs[1:], **kwargs)
+	connection = connect(kwargs.pop('netloc', None))
+	return connection.check_call(*popenargs, **kwargs)
 
 def check_output(*popenargs, **kwargs):
-	netloc = kwargs.pop('netloc', None)
-	remote_shell = kwargs.pop('remote_shell', None)
-	cmd = remote_cmd(popenargs[0], netloc, remote_shell)
-	return subprocess.check_output(cmd, *popenargs[1:], **kwargs)
+	connection = connect(kwargs.pop('netloc', None))
+	return connection.check_output(*popenargs, **kwargs)
