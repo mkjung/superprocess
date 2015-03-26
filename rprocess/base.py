9776888d6fc16350fc71471e13a522fffa56103d
Stephen Drake <steve@synergyconsultingnz.com>
2015-03-26 17:53:24 +1300

    Added PopenBase
    Added base class to facilitate extending subprocess.Popen.  PopenBase
    implements the convenience methods from subprocess as classmethods so
    that they are automatically available to subclasses.
    
    For now only the convenience methods from Python 2.7 are implemented,
    and the implementations make use of an added fail_on_error property.
    This may be split out into a separate mixin in the future, and call()
    etc implemented fully as they are in subprocess.

--- rprocess/base.py
+++ rprocess/base.py
@@ -1,5 +1,44 @@
 import subprocess
 
+class PopenBase(subprocess.Popen):
+	@classmethod
+	def call(Popen, *args, **kwargs):
+		return Popen(*args, **kwargs).wait()
+
+	@classmethod
+	def check_call(Popen, *args, **kwargs):
+		return Popen.call(*args, fail_on_error=True, **kwargs)
+
+	@classmethod
+	def check_output(Popen, *args, **kwargs):
+		out, err = Popen(
+			*args, stdout=subprocess.PIPE, fail_on_error=True, **kwargs
+		).communicate()
+		return out
+
+	def __init__(self, cmd, *args, **kwargs):
+		fail_on_error = kwargs.pop('fail_on_error', False)
+
+		super(PopenBase, self).__init__(cmd, *args, **kwargs)
+		self.cmd = cmd
+		self._fail_on_error = fail_on_error
+
+	def check(self):
+		if self.returncode:
+			raise subprocess.CalledProcessError(self.returncode, self.cmd)
+
+	def poll(self):
+		super(PopenBase, self).poll()
+		if self._fail_on_error:
+			self.check()
+		return self.returncode
+
+	def wait(self):
+		super(PopenBase, self).wait()
+		if self._fail_on_error:
+			self.check()
+		return self.returncode
+
 class LocalConnection(object):
 	def __enter__(self):
 		return self
